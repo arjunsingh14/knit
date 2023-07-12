55382461ec4e0891095aa908144243ecf27de17c
arjunsingh14 <arjunsingh140601@gmail.com>
2023-07-11 21:08:06 -0400

    Moved login oage to home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,27 +2,35 @@ import React from "react";
 import Link from "next/link";
 const page = () => {
   return (
-    <section className="flex flex-col items-center">
-      <div className="self-baseline pb-4">
+    <section className="flex flex-col md:flex-row p-5">
+      <div className="self-baseline ml-2 mx-10">
         <h1 className="text-3xl">Knit</h1>
-        <hr className=""></hr>
+        <hr />
+        <div className="w-full md:w-[275px] mt-3">
+          <p className="text-xl w-full">
+             Join Knit today and startweaving your own digital tapestry of connections, one thread at a time!
+          </p>
+        </div>
       </div>
-      <div>
-        <p className="text-l">
-          Welcome to Knit, the minimalist social media platform designed to connect people
-          through short,<br></br>concise messages. Join Knit today and start
-          weaving your own digital tapestry of connections,
-          <br></br>one thread at a time.
-        </p>
-      </div>
-      <div className="mt-5">
-        <button className="rounded-full px-4">
-          <Link href="/login">Login</Link>
-        </button>
-        <button className="rounded-full px-4">
-          <Link href="/signup">Signup</Link>
-        </button>
-      </div>
+      <form action="" className="block md:px-5 self-center mt-5 md:m-0 w-full lg:mx-10">
+        <div className="mb-5">
+          <h1 className="text-3xl">Login</h1>
+          <hr />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input type="email" id="email" name="email" className="mb-4 w-full"/>
+          <br />
+          <label htmlFor="password">Password</label>
+          <br />
+          <input type="password" id="password" name="password"  className="w-full"/>
+          <br />
+          <button className=" w-[100%] text-s mt-2 rounded-full hover:bg-[#81a48c] hover:text-white transition-colors py-2 px-3 mx-auto">
+            Login
+          </button>
+        </div>
+      </form>
     </section>
   );
 };
--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,9 +1,47 @@
+'use client'
+
 import React from 'react'
+
+
 
 const page = () => {
   return (
-    <div>signup</div>
-  )
+    <form className="block p-5">
+      <div className="mb-5">
+        <h1 className="text-xl">Login</h1>
+        <hr />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <br />
+        <input type="email" id="email" name="email" className="mb-4 w-full" />
+        <br />
+        <label htmlFor="email">Username</label>
+        <br />
+        <input type="email" id="email" name="email" className="w-full" />
+        <br />
+        <br />
+        <div className="float float-left mr-1">
+          <label htmlFor="name">First Name</label>
+          <br />
+          <input type="text" className="" />
+        </div>
+        <div className="float float-left mb-4">
+          <label htmlFor="name">Last Name</label>
+          <br />
+          <input type="text" className="" />
+        </div>
+        <br></br>
+        <label htmlFor="password">Password</label>
+        <br />
+        <input type="password" id="password" name="password" className='w-full'/>
+        <br />
+        <button className=" w-[100%] text-s mt-2 rounded-full hover:bg-[#81a48c] hover:text-white transition-colors py-2 px-3 mx-auto">
+          Signup
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default page
